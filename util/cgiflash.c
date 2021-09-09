128fc29363e79cc71b5f57d5e6b7f268ea0103a3
michal4132 <michal.b2000@onet.pl>
2021-09-10 00:51:01 +0200

    Fix esp8266 OTA and remove espfs

--- util/cgiflash.c
+++ util/cgiflash.c
@@ -14,6 +14,8 @@ Some flash handling cgi routines. Used for updating the ESPFS/OTA image.
 #include "httpd-platform.h"
 #if defined(ESP32) || defined(ESP8266)
 #include "esp32_flash.h"
+
+#include "esp_system.h"
 #include "esp_ota_ops.h"
 #include "esp_log.h"
 #include "esp_flash_partitions.h"
@@ -36,6 +38,7 @@ static int ICACHE_FLASH_ATTR checkBinHeader(void *buf) {
 #if defined(ESP32) || defined(ESP8266)
 	printf("checkBinHeader: %x %x %x\n", cd[0], ((uint16_t *)buf)[3], ((uint32_t *)buf)[0x6]);
 	if (cd[0] != 0xE9) return 0;
+        return 1; //TODO FIX CHECK
 	if (((uint16_t *)buf)[3] != 0x4008) return 0;
 	uint32_t a=((uint32_t *)buf)[0x6];
 	if (a!=0 && (a<=0x3F000000 || a>0x40400000)) return 0;
@@ -48,24 +51,15 @@ static int ICACHE_FLASH_ATTR checkBinHeader(void *buf) {
 	return 1;
 }
 
-static int ICACHE_FLASH_ATTR checkEspfsHeader(void *buf) {
-	if (memcmp(buf, "ESfs", 4)!=0) return 0;
-	return 1;
-}
-
-
 // Cgi to query which firmware needs to be uploaded next
 CgiStatus ICACHE_FLASH_ATTR cgiGetFirmwareNext(HttpdConnData *connData) {
 	if (connData->isConnectionClosed) {
 		//Connection aborted. Clean up.
 		return HTTPD_CGI_DONE;
 	}
-#if defined(ESP32) || defined(ESP8266)
 	//Doesn't matter, we have a MMU to remap memory, so we only have one firmware image.
 	uint8_t id = 0;
-#else
-	uint8_t id = system_upgrade_userbin_check();
-#endif
+
 	httpdStartResponse(connData, 200);
 	httpdHeader(connData, "Content-Type", "text/plain");
 	httpdHeader(connData, "Content-Length", "9");
@@ -85,7 +79,7 @@ CgiStatus ICACHE_FLASH_ATTR cgiGetFirmwareNext(HttpdConnData *connData) {
 //have to buffer some data because the post buffer may be misaligned, we
 //write SPI data in pages. The page size is a software thing, not
 //a hardware one.
-#ifdef ESP32
+#if defined(ESP32) || defined(ESP8266)
 #define PAGELEN 4096
 #else
 #define PAGELEN 64
@@ -93,9 +87,6 @@ CgiStatus ICACHE_FLASH_ATTR cgiGetFirmwareNext(HttpdConnData *connData) {
 
 #define FLST_START 0
 #define FLST_WRITE 1
-#if !(defined(ESP32) || defined(ESP8266))
-#define FLST_SKIP 2
-#endif
 #define FLST_DONE 3
 #define FLST_ERROR 4
 
@@ -113,23 +104,11 @@ typedef struct {
 	int filetype;
 	int flashPos;
 	char pageData[PAGELEN];
-#if !(defined(ESP32) || defined(ESP8266))
-	int pagePos;
-#endif
 	int address;
 	int len;
 	int skip;
 	const char *err;
 } UploadState;
-
-#if !(defined(ESP32) || defined(ESP8266))
-typedef struct __attribute__((packed)) {
-	char magic[4];
-	char tag[28];
-	int32_t len1;
-	int32_t len2;
-} OtaHeader;
-#endif
 
 static void cgiJsonResponseCommon(HttpdConnData *connData, cJSON *jsroot){
 	char *json_string = NULL;
@@ -150,7 +129,6 @@ static void cgiJsonResponseCommon(HttpdConnData *connData, cJSON *jsroot){
     cJSON_Delete(jsroot);
 }
 
-#if defined(ESP32) || defined(ESP8266)
 CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 	CgiUploadFlashDef *def=(CgiUploadFlashDef*)connData->cgiArg;
 	UploadState *state=(UploadState *)connData->cgiData;
@@ -194,6 +172,7 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 			state->err="Premature end";
 		}
 		state->update_partition = NULL;
+		state->update_handle = 0;
 		// check arg partition name
 		char arg_partition_buf[16] = "";
 		int len;
@@ -223,11 +202,7 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 	while (dataLen!=0) {
 		if (state->state==FLST_START) {
 			//First call. Assume the header of whatever we're uploading already is in the POST buffer.
-			if (def->type==CGIFLASH_TYPE_FW && memcmp(data, "EHUG", 4)==0) {
-				state->err="Combined flash images are unneeded/unsupported on ESP32!";
-				state->state=FLST_ERROR;
-				ESP_LOGE(TAG, "Combined flash image not supported on ESP32!");
-			} else if (def->type==CGIFLASH_TYPE_FW && checkBinHeader(connData->post.buff)) {
+			if (def->type==CGIFLASH_TYPE_FW && checkBinHeader(connData->post.buff)) {
 			    if (state->update_partition == NULL)
 			    {
 					ESP_LOGE(TAG, "update_partition not found!");
@@ -248,7 +223,7 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 					err = esp_ota_begin(state->update_partition, OTA_SIZE_UNKNOWN, &state->update_handle);
 						*pst = old_subtype; // put the value back to original now
 					}
-					else 
+					else
 #endif
 					{
 						err = esp_ota_begin(state->update_partition, OTA_SIZE_UNKNOWN, &state->update_handle);
@@ -267,15 +242,6 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 						state->len = connData->post.len;
 					}
 			    }
-			} else if (def->type==CGIFLASH_TYPE_ESPFS && checkEspfsHeader(connData->post.buff)) {
-				if (connData->post.len > def->fwSize) {
-					state->err="Firmware image too large";
-					state->state=FLST_ERROR;
-				} else {
-					state->len=connData->post.len;
-					state->address=def->fw1Pos;
-					state->state=FLST_WRITE;
-				}
 			} else {
 				state->err="Invalid flash image type!";
 				state->state=FLST_ERROR;
@@ -305,13 +271,6 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 			dataLen=0;
 		}
 	}
-
-#if 0
-	//TODO: maybe use ESP_LOGD() here in the future
-	printf("post->len %d, post->received %d\n", connData->post->len,
-		connData->post->received);
-	printf("state->len %d, state->address: %d\n", state->len, state->address);
-#endif
 
 	if  (connData->post.len == connData->post.received) {
 		cJSON *jsroot = cJSON_CreateObject();
@@ -343,183 +302,13 @@ CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
 	return HTTPD_CGI_MORE;
 }
 
-#else
-
-CgiStatus ICACHE_FLASH_ATTR cgiUploadFirmware(HttpdConnData *connData) {
-	CgiUploadFlashDef *def=(CgiUploadFlashDef*)connData->cgiArg;
-	UploadState *state=(UploadState *)connData->cgiData;
-	int len;
-	char buff[128];
-
-	if (connData->isConnectionClosed) {
-		//Connection aborted. Clean up.
-		if (state!=NULL) free(state);
-		return HTTPD_CGI_DONE;
-	}
-
-	if (state==NULL) {
-		//First call. Allocate and initialize state variable.
-		ESP_LOGE(TAG, "Firmware upload cgi start");
-		state=malloc(sizeof(UploadState));
-		if (state==NULL) {
-			ESP_LOGE(TAG, "Can't allocate firmware upload struct");
-			return HTTPD_CGI_DONE;
-		}
-		memset(state, 0, sizeof(UploadState));
-		state->state=FLST_START;
-		connData->cgiData=state;
-		state->err="Premature end";
-	}
-
-	char *data=connData->post->buff;
-	int dataLen=connData->post->buffLen;
-
-	while (dataLen!=0) {
-		if (state->state==FLST_START) {
-			//First call. Assume the header of whatever we're uploading already is in the POST buffer.
-			if (def->type==CGIFLASH_TYPE_FW && memcmp(data, "EHUG", 4)==0) {
-				//Type is combined flash1/flash2 file
-				OtaHeader *h=(OtaHeader*)data;
-				strncpy(buff, h->tag, 27);
-				buff[27]=0;
-				if (strcmp(buff, def->tagName)!=0) {
-					ESP_LOGE(TAG, "OTA tag mismatch! Current=`%s` uploaded=`%s`",
-										def->tagName, buff);
-					len=httpdFindArg(connData->getArgs, "force", buff, sizeof(buff));
-					if (len!=-1 && atoi(buff)) {
-						ESP_LOGE(TAG, "Forcing firmware flash");
-					} else {
-						state->err="Firmware not intended for this device!\n";
-						state->state=FLST_ERROR;
-					}
-				}
-				if (state->state!=FLST_ERROR && connData->post->len > def->fwSize*2+sizeof(OtaHeader)) {
-					state->err="Firmware image too large";
-					state->state=FLST_ERROR;
-				}
-				if (state->state!=FLST_ERROR) {
-					//Flash header seems okay.
-					dataLen-=sizeof(OtaHeader); //skip header when parsing data
-					data+=sizeof(OtaHeader);
-					if (system_upgrade_userbin_check()==1) {
-						ESP_LOGD(TAG, "Flashing user1.bin from ota image");
-						state->len=h->len1;
-						state->skip=h->len2;
-						state->state=FLST_WRITE;
-						state->address=def->fw1Pos;
-					} else {
-						ESP_LOGD(TAG, "Flashing user2.bin from ota image");
-						state->len=h->len2;
-						state->skip=h->len1;
-						state->state=FLST_SKIP;
-						state->address=def->fw2Pos;
-					}
-				}
-			} else if (def->type==CGIFLASH_TYPE_FW && checkBinHeader(connData->post->buff)) {
-				if (connData->post->len > def->fwSize) {
-					state->err="Firmware image too large";
-					state->state=FLST_ERROR;
-				} else {
-					state->len=connData->post->len;
-					state->address=def->fw1Pos;
-					state->state=FLST_WRITE;
-				}
-			} else if (def->type==CGIFLASH_TYPE_ESPFS && checkEspfsHeader(connData->post->buff)) {
-				if (connData->post->len > def->fwSize) {
-					state->err="Firmware image too large";
-					state->state=FLST_ERROR;
-				} else {
-					state->len=connData->post->len;
-					state->address=def->fw1Pos;
-					state->state=FLST_WRITE;
-				}
-			} else {
-				state->err="Invalid flash image type!";
-				state->state=FLST_ERROR;
-				ESP_LOGE(TAG, "Did not recognize flash image type");
-			}
-		} else if (state->state==FLST_SKIP) {
-			//Skip bytes without doing anything with them
-			if (state->skip>dataLen) {
-				//Skip entire buffer
-				state->skip-=dataLen;
-				dataLen=0;
-			} else {
-				//Only skip part of buffer
-				dataLen-=state->skip;
-				data+=state->skip;
-				state->skip=0;
-				if (state->len) state->state=FLST_WRITE; else state->state=FLST_DONE;
-			}
-		} else if (state->state==FLST_WRITE) {
-			//Copy bytes to page buffer, and if page buffer is full, flash the data.
-			//First, calculate the amount of bytes we need to finish the page buffer.
-			int lenLeft=PAGELEN-state->pagePos;
-			if (state->len<lenLeft) lenLeft=state->len; //last buffer can be a cut-off one
-			//See if we need to write the page.
-			if (dataLen<lenLeft) {
-				//Page isn't done yet. Copy data to buffer and exit.
-				memcpy(&state->pageData[state->pagePos], data, dataLen);
-				state->pagePos+=dataLen;
-				state->len-=dataLen;
-				dataLen=0;
-			} else {
-				//Finish page; take data we need from post buffer
-				memcpy(&state->pageData[state->pagePos], data, lenLeft);
-				data+=lenLeft;
-				dataLen-=lenLeft;
-				state->pagePos+=lenLeft;
-				state->len-=lenLeft;
-				//Erase sector, if needed
-				if ((state->address&(SPI_FLASH_SEC_SIZE-1))==0) {
-					spi_flash_erase_sector(state->address/SPI_FLASH_SEC_SIZE);
-				}
-				//Write page
-				//httpd_printf("Writing %d bytes of data to SPI pos 0x%x...\n", state->pagePos, state->address);
-				spi_flash_write(state->address, (uint32 *)state->pageData, state->pagePos);
-				state->address+=PAGELEN;
-				state->pagePos=0;
-				if (state->len==0) {
-					//Done.
-					if (state->skip) state->state=FLST_SKIP; else state->state=FLST_DONE;
-				}
-			}
-		} else if (state->state==FLST_DONE) {
-			ESP_LOGE(TAG, "%d bogus bytes received after data received", dataLen);
-			//Ignore those bytes.
-			dataLen=0;
-		} else if (state->state==FLST_ERROR) {
-			//Just eat up any bytes we receive.
-			dataLen=0;
-		}
-	}
-
-	if (connData->post->len==connData->post->received) {
-		//We're done! Format a response.
-		ESP_LOGD(TAG, "Upload done. Sending response");
-		httpdStartResponse(connData, state->state==FLST_ERROR?400:200);
-		httpdHeader(connData, "Content-Type", "text/plain");
-		httpdEndHeaders(connData);
-		if (state->state!=FLST_DONE) {
-			httpdSend(connData, "Firmware image error:", -1);
-			httpdSend(connData, state->err, -1);
-			httpdSend(connData, "\n", -1);
-		}
-		free(state);
-		return HTTPD_CGI_DONE;
-	}
-
-	return HTTPD_CGI_MORE;
-}
-#endif
-
 static HttpdPlatTimerHandle resetTimer;
 
 static void ICACHE_FLASH_ATTR resetTimerCb(void *arg) {
-#if !(defined(ESP32) || defined(ESP8266))
-	system_upgrade_flag_set(UPGRADE_FLAG_FINISH);
-	system_upgrade_reboot();
-#else
+#if defined(ESP8266)
+  esp_restart();
+#endif
+#if defined(ESP32)
 	esp32flashRebootIntoOta();
 #endif
 }
@@ -594,16 +383,13 @@ CgiStatus ICACHE_FLASH_ATTR cgiEraseFlash(HttpdConnData *connData) {
 	int len;
 //// HTTP GET queryParameter "partition" : string
     len=httpdFindArg(connData->getArgs, "partition", arg_partition_buf, sizeof(arg_partition_buf));
-    if (len > 0)
-    {
+    if (len > 0){
     	ESP_LOGD(TAG, "Erase command recvd. for partition with name: %s", arg_partition_buf);
     	wanted_partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA,ESP_PARTITION_SUBTYPE_ANY,arg_partition_buf);
     	err = esp_partition_erase_range(wanted_partition, 0, wanted_partition->size);
     	if (err != ESP_OK) {
     		ESP_LOGE(TAG, "erase partition failed! err=0x%x", err);
-    	}
-    	else
-    	{
+    	}else{
     		ESP_LOGW(TAG, "Data partition: %s is erased now!  Must reboot to reformat it!", wanted_partition->label);
     		cJSON_AddStringToObject(jsroot, "erased", wanted_partition->label);
     	}
@@ -619,20 +405,20 @@ CgiStatus ICACHE_FLASH_ATTR cgiEraseFlash(HttpdConnData *connData) {
  *  Warning- this takes a long time to execute and dumps a bunch of stuff to the console!
  *  todo: find a faster method to veryify an APP
  */
-static int check_partition_valid_app(const esp_partition_t *partition)
-{
+static int check_partition_valid_app(const esp_partition_t *partition){
     if (partition == NULL) {
         return 0;
     }
 
-    esp_image_metadata_t data;
-    const esp_partition_pos_t part_pos = {
-        .offset = partition->address,
-        .size = partition->size,
-    };
 #if defined(ESP32)
-//TODO
-//#if defined(ESP32) || defined(ESP8266)
+//TODO VERIFY on esp8266
+
+	esp_image_metadata_t data;
+	const esp_partition_pos_t part_pos = {
+			.offset = partition->address,
+			.size = partition->size,
+	};
+
 	if (esp_image_verify(ESP_IMAGE_VERIFY_SILENT, &part_pos, &data) != ESP_OK) {
         return 0;  // partition does not hold a valid app
     }
